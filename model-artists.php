ee4e4b9b8128b7da168c5ff694a3bee9acdb4a28
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-06 22:31:41 -0500

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -14,4 +14,45 @@ function selectArtists() {
     }
 }
 
+function insertArtist($Name, $Genre) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO artists (Name, Genre) VALUES (?, ?)");
+        $stmt->bind_param("ss", $Name, $Genre);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCourse($Name, $Genre, $AID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE artists SET Name = ?, Genre = ? WHERE ArtistID = ?");
+        $stmt->bind_param("ssi", $Name, $Genre, $AID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCourse($AID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM artists WHERE ArtistID = ?");
+        $stmt->bind_param("i", $AID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
