53a5035902d26239433ab92e6dc860e0fe41f41e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-07 12:53:30 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -5,6 +5,11 @@ require_once("model-artists.php");
 $pageTitle = "Artists";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+  
+  }
+}
+
 $artists = selectArtists();
 
 include "view-artists.php";
