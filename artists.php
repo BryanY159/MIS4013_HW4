4b91434bf2c08c269ace57004a4c9090d357be08
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-07 12:56:24 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -6,7 +6,10 @@ $pageTitle = "Artists";
 include "view-header.php";
 
 if (isset($_POST['actionType'])) {
-  
+  switch ($_POST['actionType']) {
+    case "Add"
+      break;
+  }
 }
 
 $artists = selectArtists();
