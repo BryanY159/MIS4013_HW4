83f5518354c3ae81eb4991aecba4366b909a5f66
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-07 13:26:31 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Added </div>';
       }
       break;
+    case "Delete":
+      if (deleteArtist($_POST['artistName'])) {
+        echo '<div class="alert alert-success" role="alert"> Artist Deleted Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Deleted </div>';
+      }
+      break;
   }
 }
 
