c15ec7ae7b0015cb91ae56d1c3fc225842498a3d
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-07 13:08:57 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertArtist($_POST['artistName'], $_POST['artistGenre']);
+      if (insertArtist($_POST['artistName'], $_POST['artistGenre'])) {
+        echo '<div class="alert alert-success" role="alert"> Artist Added Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Added </div>';
+      }
       break;
   }
 }
