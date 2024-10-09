9451d33d65e4f1817b3ee8662c0af88e8296c702
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-09 13:39:47 -0500

    Update songs-by-artist.php

--- songs-by-artist.php
+++ songs-by-artist.php
@@ -5,6 +5,38 @@ require_once("model-songs-by-artist.php");
 $pageTitle = "Songs by Artist";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    /*
+    case "Add":
+      if (insertSong($_POST['SongTitle'], $_POST['SongGenre'], $_POST['ArtistID'])) {
+        echo '<div class="alert alert-success" role="alert"> Song Added Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Song Not Added </div>';
+      }
+      break;
+    */
+    /*
+    case "Edit":
+      if (updateSong($_POST['SongTitle'], $_POST['SongGenre'], $_POST['ArtistID'], $_POST['SongID'])) {
+        echo '<div class="alert alert-success" role="alert"> Song Edited Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Song Not Edited </div>';
+      }
+      break;
+    */
+    /*
+    case "Delete":
+      if (deleteSong($_POST['SongID'])) {
+        echo '<div class="alert alert-success" role="alert"> Song Deleted Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Song Not Deleted </div>';
+      }
+      break;
+    */
+  }
+}
+
 $songs = selectSongsByArtist($_GET['id']);
 
 include "view-songs-by-artist.php";
