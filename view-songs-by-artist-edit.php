943a81532ba19280d7453d77baf5bca39672e04a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-09 18:16:14 -0500

    Update view-songs-by-artist-edit.php

--- view-songs-by-artist-edit.php
+++ view-songs-by-artist-edit.php
@@ -1,1 +1,3 @@
 <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editModal<?php echo $song['SongID']; ?>">Edit</button>
+
+<!-- Was getting errors before. Add modal in later. -->
