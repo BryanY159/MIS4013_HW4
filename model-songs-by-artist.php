e158231f69cd2779bf6bfdfe9d178ddbb4806266
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-09 13:36:23 -0500

    Update model-songs-by-artist.php

--- model-songs-by-artist.php
+++ model-songs-by-artist.php
@@ -15,4 +15,46 @@ function selectSongsByArtist($AID) {
     }
 }
 
+function insertSong($Title, $Genre, $AID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO songs (Title, Genre, ArtistID) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $Title, $Genre, $AID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSong($Title, $Genre, $,AID, $SID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE songs SET Name = ?, Genre = ?, ArtistID = ? WHERE SongID = ?");
+        $stmt->bind_param("ssii", $Name, $Genre, $AID, $SID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSong($SID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM songs WHERE SongID = ?");
+        $stmt->bind_param("i", $SID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
