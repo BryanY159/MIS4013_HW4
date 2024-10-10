82b7290a3715aef6e6a7ff519a2d166784b01a1e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-09 19:22:38 -0500

    Update users.php

--- users.php
+++ users.php
@@ -5,6 +5,32 @@ require_once("model-users.php");
 $pageTitle = "Users";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertUser($_POST['Username'], $_POST['SubscriptionType'])) {
+        echo '<div class="alert alert-success" role="alert"> Artist Added Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Added </div>';
+      }
+      break;
+    case "Edit":
+      if (updateUser($_POST['Username'], $_POST['SubscriptionType'], $_POST['UserID'])) {
+        echo '<div class="alert alert-success" role="alert"> Artist Edited Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Edited </div>';
+      }
+      break;
+    case "Delete":
+      if (deleteUser($_POST['UserID'])) {
+        echo '<div class="alert alert-success" role="alert"> Artist Deleted Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Artist Not Deleted </div>';
+      }
+      break;
+  }
+}
+
 $users = selectUsers();
 
 include "view-users.php";
