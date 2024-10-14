26f631028f21485b6051ca7feac05ebc9bf6e1ae
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-14 13:15:26 -0500

    Update view-playlists-with-songs.php

--- view-playlists-with-songs.php
+++ view-playlists-with-songs.php
@@ -1,20 +1,32 @@
 <h1>Playlists with Songs</h1>
 
-<div class="card-group">
-    <?php while($playlist = $playlists->fetch_assoc()) { ?>                                                 
-      <div class="card">
-        <div class="card-body">
-          <h2 class="card-title"><?php echo $playlist['PlaylistName']; ?> </h2>
-          <p class="card-text">
-            <ul class="list-group">
-              <?php
-                $songs = selectSongsByPlaylist($playlist['PlaylistName']);
-                while ($song = $songs->fetch_assoc()) { ?>
-                  <li class="list-group-item"><?php echo $song['SongID']; ?> - <?php echo $song['Title']; ?></li>
-              <?php } ?>
-            </ul>
-          </p>
-        </div>
-      </div>
-    <?php } ?>
+<div class="container">
+    <div class="row">
+        <?php 
+        $count = 0;
+        while($playlist = $playlists->fetch_assoc()) { 
+            if ($count % 4 == 0 && $count != 0) { 
+                // Close the current row and start a new one every 4 cards
+                echo '</div><div class="row">';
+            }
+            $count++;
+        ?>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title"><?php echo $playlist['PlaylistName']; ?></h2>
+                        <p class="card-text">
+                            <ul class="list-group">
+                                <?php
+                                $songs = selectSongsByPlaylist($playlist['PlaylistName']);
+                                while ($song = $songs->fetch_assoc()) { ?>
+                                    <li class="list-group-item"><?php echo $song['SongID']; ?> - <?php echo $song['Title']; ?></li>
+                                <?php } ?>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 </div>
