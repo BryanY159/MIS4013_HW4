bbcf47ab023fa34e78b66311bbff88b547036c87
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-14 19:03:18 -0500

    Update playlists-by-user.php

--- playlists-by-user.php
+++ playlists-by-user.php
@@ -5,6 +5,32 @@ require_once("model-playlists-by-user.php");
 $pageTitle = "Playlists by User";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlaylist($_POST['UserID'], $_POST['SongID'], $_POST['PlaylistName'])) {
+        echo '<div class="alert alert-success" role="alert"> Playlist Added Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Playlist Not Added </div>';
+      }
+      break;
+    case "Edit":
+      if (updatePlaylist($_POST['PlaylistName'])) {
+        echo '<div class="alert alert-success" role="alert"> Playlist Edited Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Playlist Not Edited </div>';
+      }
+      break;
+    case "Delete":
+      if (deletePlaylist($_POST['PlaylistName'])) {
+        echo '<div class="alert alert-success" role="alert"> Playlist Deleted Successfully </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error: Playlist Not Deleted </div>';
+      }
+      break;
+  }
+}
+
 $playlists = selectPlaylistsByUser($_POST['UID']);
 
 include "view-playlists-by-user.php";
