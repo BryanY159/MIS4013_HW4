5c99222a239fbbe09f694bd7d83c6e2a47a3fdfb
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-15 11:17:50 -0500

    Update view-playlists-with-songs-add-playlist.php

--- view-playlists-with-songs-add-playlist.php
+++ view-playlists-with-songs-add-playlist.php
@@ -17,7 +17,7 @@
             <input type="text" class="form-control" id="PlaylistName" name="PlaylistName">
           </div>
           <div class="mb-3">
-            <label for="SongID" class="form-label">Username</label>
+            <label for="UserID" class="form-label">Username</label>
             <?php
               $userList = selectUsersForInput();
             ?>
