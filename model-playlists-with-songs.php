a5954d2e9a8ccc3f86376ce776bc53b37d6b558e
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-15 11:01:36 -0500

    Update model-playlists-with-songs.php

--- model-playlists-with-songs.php
+++ model-playlists-with-songs.php
@@ -29,10 +29,38 @@ function selectSongsByPlaylist($PName) {
     }
 }
 
+function insertPlaylist($UID, $SID, $PlaylistName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO playlists (UserID, SongID, PlaylistName) Values (?, ?, ?)");
+        $stmt->bind_param("iis", $UID, $SID, $PlaylistName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectSongsForInput() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT SongID, Title FROM songs ORDER BY SongID");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectUsersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT UserID, Username FROM users ORDER BY UserID");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
