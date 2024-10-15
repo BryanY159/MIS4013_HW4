5d49f17a12f507a21a88862b20629223ead4551a
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-10-15 11:29:58 -0500

    Update playlists-with-songs.php

--- playlists-with-songs.php
+++ playlists-with-songs.php
@@ -14,24 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert"> Error: Playlist Not Added </div>';
       }
       break;
-    /*
-    case "Add":
-      if (insertSong()) {
-        echo '<div class="alert alert-success" role="alert"> Song Add Successfully </div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error: Song Not Added </div>';
-      }
-      break;
-    */
-    /*
     case "Delete":
-      if (deleteSong()) {
+      if (deletePlaylist($_POST['SongID'], $_POST['PlaylistName'])) {
         echo '<div class="alert alert-success" role="alert"> Song Deleted Successfully </div>';
       } else {
         echo '<div class="alert alert-danger" role="alert"> Error: Song Not Deleted </div>';
       }
       break;
-    */
   }
 }
 
