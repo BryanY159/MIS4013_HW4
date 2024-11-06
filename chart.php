81a13cc39ead51a40715b668853e0d9d98e0a4e7
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-06 13:05:39 -0600

    Create chart.php

---
+++ chart.php
@@ -0,0 +1,64 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+
+<h1>Chart</h1>
+
+<button type="button" class="btn btn-primary" id="tutorial-button" onclick="startIntro()">Start Tutorial</button>
+
+
+
+<script>
+  function startIntro() {
+    const step = parseInt(localStorage.getItem('currentStep') || 0);
+
+    const steps = [
+      {
+        intro: "Welcome to HW6 Dr. Bellah! Click through this tutorial to find where my JavaScript libraries are implemented!",
+      },
+      {
+        element: '#tutorial-button',
+        intro: "My first library is Intro.js. I will use it on each of the prompted pages to explain the JavaScript library. It allows for interactive tutorials like this.",
+        position: 'bottom'
+      },
+      {
+        element: '#artists-page',
+        intro: "My second and third library will be shown here on the Artists Page. I used SweetAlerts2 and Toastify.js to change the button alerts for adding, editing, and deleting (I only did it on this page to avoid just copying code).",
+        position: 'bottom'
+      },
+      {
+        element: '#chart-page',
+        intro: "My fourth library will be shown here on the new Chart Page. I used Chart.js to create a chart visualization from the database.",
+        position: 'bottom'
+      },
+      {
+        intro: "Navigate to these pages to find more tutorial buttons! Thank you!",
+      }
+    ];
+
+    introJs().setOptions({
+      initialStep: step,
+      steps: steps
+    })
+    .start()
+    .onchange(function() {
+      const currentStep = introJs().currentStep();
+      localStorage.setItem('currentStep', currentStep);
+    })
+    .oncomplete(function() {
+      localStorage.setItem('currentStep', 0); // Reset for next page
+      window.location.href = 'artists.php'; // Redirect to Songs page
+    })
+    .onexit(function() {
+      localStorage.removeItem('currentStep');  // Clear progress if exited
+    });
+  }
+</script>
+
+</body>
+</html>
+
+<?php
+include "view-footer.php";
+?>
