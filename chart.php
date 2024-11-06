db1d4d9273d37e24314d4e1952dd09b509d39e06
BryanY159 <98123174+BryanY159@users.noreply.github.com>
2024-11-06 13:37:13 -0600

    Update chart.php

--- chart.php
+++ chart.php
@@ -6,34 +6,35 @@ include "view-header.php";
 <h1>Chart</h1>
         <button type="button" class="btn btn-primary" id="tutorial-button" onclick="startIntro()">Start Tutorial</button>
 
-        <!-- ECharts container -->
         <div id="myChart" style="width: 600px; height: 400px;"></div>
 
-        <script>
-            // ECharts initialization for a bar chart
-            const chartDom = document.getElementById('myChart');
-            const myChart = echarts.init(chartDom);
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const chartDom = document.getElementById('myChart');
+        const myChart = echarts.init(chartDom);
 
-            const option = {
-                title: {
-                    text: 'Sample Bar Chart'
-                },
-                tooltip: {},
-                xAxis: {
-                    type: 'category',
-                    data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [{
-                    name: 'Data',
-                    type: 'bar',
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                }]
-            };
+        const option = {
+            title: {
+                text: 'Sample Bar Chart'
+            },
+            tooltip: {},
+            xAxis: {
+                type: 'category',
+                data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                name: 'Data',
+                type: 'bar',
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }]
+        };
 
-            myChart.setOption(option);
+        myChart.setOption(option);
+    });
+    </script>
 
   // Intro.js tutorial steps
   function startIntro() {
